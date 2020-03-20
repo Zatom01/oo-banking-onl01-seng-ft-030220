4f9e41df4898821ee4709b1162b53bdeaacedd69
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-20 07:23:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status 
+  
+  def initialize(sender,receiver,amount)
+    @sender=sender
+    @receiver=receiver
+    @status="pending"
+    
+  
+  
+  
+  
 end
